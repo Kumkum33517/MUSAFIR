e353507eb19fde5fb372aafd28a97cbcbb4d20c4
Kumkum <kumkum33517@gmail.com>
2026-09-18 21:06:08 +0530

    fix: revert favicon to png for Expo build

--- app.config.ts
+++ app.config.ts
@@ -83,7 +83,7 @@ const config: ExpoConfig = {
   web: {
     bundler: "metro",
     output: "static",
-    favicon: "./assets/images/favicon.svg",
+    favicon: "./assets/images/favicon.png",
   },
   plugins: [
     "expo-router",
